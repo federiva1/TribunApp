ec2ec5ce846305ac3589945199a43957f7f98c74
Fede Riva <federico.rivadeneira92@gmail.com>
2026-04-25 16:18:50 -0300

    feat: centralizar credenciales Supabase en js/config.js

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://ieujdkthaoujtnndkiqo.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlldWpka3RoYW91anRubmRraXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMTEwOTYsImV4cCI6MjA5MTc4NzA5Nn0.FDzVx-W3v48Eq9_hSzsghP1Q8ES4e70S7MN2koGT5wY';
